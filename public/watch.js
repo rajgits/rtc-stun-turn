2499f6a43b647b97b7d71fb349673c36d610b7f9
Bharathraj <61880265+rajgits@users.noreply.github.com>
2020-08-14 23:13:06 +0400

    Update watch.js

--- public/watch.js
+++ public/watch.js
@@ -6,9 +6,9 @@ const config = {
         "urls": "stun:15.206.150.88:443",
       },
        { 
-        "urls": "turn:15.206.150.88?transport=tcp",
-        "username": "stunscope",
-        "credential": "stonescope@123" 
+        "urls": "",
+        "username": "",
+        "credential": "" 
        }
   ]
 };
@@ -56,6 +56,7 @@ socket.on("broadcaster", () => {
 socket.on("disconnectPeer", () => {
   peerConnection.close();
 });
+
 
 window.onunload = window.onbeforeunload = () => {
   socket.close();
